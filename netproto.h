f9d98bdab27ffbb34be0c29327ee9229c389bc56
haylemhcb <52484087+haylemhcb@users.noreply.github.com>
2019-07-11 03:23:14 -0400

    Add files via upload

--- netproto.h
+++ netproto.h
@@ -1,6 +1,14 @@
 #ifndef NETPROTO_H
 #define NETPROTO_H
 
+int contador = 1; /* Contador de reintentos internet fallida */
+int internetok = 0;
+
+struct APIHACK_VARS
+{
+   int traffic; /* Parar 0 o activar 1 el generador de trafico */
+} VARS;
+
 struct WIRELESS
 {
   char rate[20];
@@ -21,6 +29,8 @@ struct CAPTURE
   char cmd[1000]; /* comando a ejecutar */
   pthread_t id;
   char *ip[20];
+  int timeout;
+  int working;
 };
 
 struct SNET
@@ -31,13 +41,28 @@ struct SNET
   char mtu[20];
   char gateway[20];
   char resolvfile[20];
+
   struct WIRELESS w;
   struct CAPTURE cap;
 };
 
+struct SDOWNLOAD
+{
+ char src[1000];
+ char dst[1000];
+};
+
 typedef struct SNET NET;
 NET net;
+typedef struct SDOWNLOAD DOWNLOAD;
+DOWNLOAD download;
 
+
+
+DOWNLOAD *download_new(void);
+void download_set_src(DOWNLOAD *dw, const char *src);
+void download_set_dst(DOWNLOAD *dw,const char *dst);
+void download_file(DOWNLOAD *dw);
 
 NET *net_new(void);
 void net_set_interf(NET *n, const char *interf);
@@ -46,7 +71,7 @@ void net_wifi_set_ssid(NET *n, const char *ssid);
 void net_wifi_asoc_open(NET *nt);
 void net_wifi_deasoc(NET *nt);
 void net_wifi_mode_managed(NET *nt);
-void * net_wifi_monitor_off(NET *nt);
+void net_wifi_monitor_off(NET *nt);
 void net_dump(NET *n);
 char * net_wifi_get_ssid(NET *n);
 char * net_wifi_get_signal(NET *n);
@@ -74,5 +99,12 @@ const char **net_get_dns_list();
 void sys_pkill(const char *process);
 const char *net_cap_get_ip(NET *nt);
 const char *net_cap_get_mac(NET *nt);
+void net_cap_set_timeout(NET *nt, int seg);
 const char *net_cap_get_bssid(NET *nt); /* Obtiene bssid a la que se asocia la victima */
+int net_keep_traffic_active(void);
+void sys_exec_timeout(const char *args[] , int seg);
+void net_stop_traffic_gen(void);
+void net_clean_dns_file(NET *nt);
+int net_isdown_interf(NET *nt);
+int net_is_internet(NET *nt);
 #endif
